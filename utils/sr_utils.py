7f43ebf50770d706f70cbee29e52d1fae2cf7105
Anuj Pahuja <apnoob.13@gmail.com>
2020-01-06 02:21:43 -0500

    Update metric calculation

--- utils/sr_utils.py
+++ utils/sr_utils.py
@@ -1,4 +1,22 @@
 from .common_utils import *
+from skimage.filters import gaussian
+import cv2
+from skimage.measure import compare_psnr, compare_ssim
+
+def rgb2ycbcr(im_rgb):
+    im_rgb = im_rgb.astype(np.float32)
+    im_ycrcb = cv2.cvtColor(im_rgb, cv2.COLOR_RGB2YCR_CB)
+    im_ycbcr = im_ycrcb[:,:,(0,2,1)].astype(np.float32)
+    im_ycbcr[:,:,0] = (im_ycbcr[:,:,0]*(235-16)+16)/255.0 #to [16/255, 235/255]
+    im_ycbcr[:,:,1:] = (im_ycbcr[:,:,1:]*(240-16)+16)/255.0 #to [16/255, 240/255]
+    return im_ycbcr
+
+def compare_psnr_y(x, y):
+    return compare_psnr(rgb2ycbcr(x.transpose(1,2,0))[:,:,0], rgb2ycbcr(y.transpose(1,2,0))[:,:,0])
+
+def compare_ssim_y(x, y):
+    return compare_ssim(rgb2ycbcr(x.transpose(1,2,0))[:,:,0], rgb2ycbcr(y.transpose(1,2,0))[:,:,0])
+
 
 def put_in_center(img_np, target_size):
     img_out = np.zeros([3, target_size[0], target_size[1]])
@@ -65,6 +83,131 @@ def load_LR_HR_imgs_sr(fname, imsize, factor, enforse_div32=None):
                 'HR_np': img_HR_np
            }
 
+def get_baselines_d(img_LR_pil, img_HR_pil):
+    '''Gets `bicubic`, sharpened bicubic and `nearest` baselines.'''
+    img_bicubic_pil = img_LR_pil.resize(img_HR_pil.size, Image.BICUBIC)
+    img_bicubic_np = np.array(img_bicubic_pil)
+
+    #img_nearest_pil = img_LR_pil.resize(img_HR_pil.size, Image.NEAREST)
+    #img_nearest_np = np.array(img_nearest_pil)
+    
+    img_lanczos_pil = img_LR_pil.resize(img_HR_pil.size, Image.LANCZOS)
+    img_lanczos_np = np.array(img_lanczos_pil)
+
+    #img_bic_sharp_pil = img_bicubic_pil.filter(PIL.ImageFilter.UnsharpMask())
+    #img_bic_sharp_np = pil_to_np(img_bic_sharp_pil)
+
+    return img_bicubic_np, img_lanczos_np
+
+
+def load_LR_HR_depth_sr(fname, imsize, factor, enforse_div32=None, blur=False):
+    '''Loads an image, resizes it, center crops and downscales.
+
+    Args: 
+        fname: path to the image
+        imsize: new size for the image, -1 for no resizing
+        factor: downscaling factor
+        enforse_div32: if 'CROP' center crops an image, so that its dimensions are divisible by 32.
+    '''
+    img_orig_np = np.load(fname)
+    min_val = np.min(img_orig_np)
+    max_val = np.max(img_orig_np)
+    img_orig_np_sharp = (img_orig_np - min_val)/(max_val - min_val)
+    img_orig_pil_sharp = Image.fromarray(img_orig_np_sharp)
+    img_orig_pil = img_orig_pil_sharp
+    img_orig_np = img_orig_np_sharp
+    
+    if blur:
+        img_orig_np = gaussian(img_orig_np_sharp, sigma=2, preserve_range=True)
+        img_orig_pil = Image.fromarray(img_orig_np)
+        #img_orig_pil = img_orig_pil_sharp.filter(ImageFilter.GaussianBlur(radius=factor//2))
+        #img_orig_np = np.array(img_orig_pil)
+        #img_orig_np = np.transpose(img_orig_np, (2, 0, 1))
+    
+    # For comparison with GT
+    if enforse_div32 == 'CROP':
+        new_size = (img_orig_pil.size[0] - img_orig_pil.size[0] % 32, 
+                    img_orig_pil.size[1] - img_orig_pil.size[1] % 32)
+
+        bbox = [
+                (img_orig_pil.size[0] - new_size[0])/2, 
+                (img_orig_pil.size[1] - new_size[1])/2,
+                (img_orig_pil.size[0] + new_size[0])/2,
+                (img_orig_pil.size[1] + new_size[1])/2,
+        ]
+
+        img_HR_pil = img_orig_pil.crop(bbox)
+        img_HR_np = pil_to_np(img_HR_pil)
+    else:
+        img_HR_pil, img_HR_np = img_orig_pil, img_orig_np
+        
+    LR_size = [
+               img_HR_pil.size[0] // factor, 
+               img_HR_pil.size[1] // factor
+    ]
+
+    img_LR_pil = img_HR_pil.resize(LR_size, Image.ANTIALIAS)
+    img_LR_np = np.array(img_LR_pil)
+
+    print('HR and LR resolutions: %s, %s' % (str(img_HR_pil.size), str (img_LR_pil.size)))
+
+    return {
+                'orig_pil': img_orig_pil_sharp,
+                'orig_np':  img_orig_np_sharp,
+                'LR_pil':  img_LR_pil, 
+                'LR_np': img_LR_np,
+                'HR_pil':  img_HR_pil, 
+                'HR_np': img_HR_np
+           }
+
+def load_LR_HR_imgs_dataset(fname, imsize, factor, enforse_div32=None):
+    '''Loads an image, resizes it, center crops and downscales.
+
+    Args: 
+        fname: path to the image
+        imsize: new size for the image, -1 for no resizing
+        factor: downscaling factor
+        enforse_div32: if 'CROP' center crops an image, so that its dimensions are divisible by 32.
+    '''
+    img_orig_pil, img_orig_np = get_image(fname, -1)
+
+    if imsize != -1:
+        img_orig_pil, img_orig_np = get_image(fname, imsize)
+        
+    # For comparison with GT
+    if enforse_div32 == 'CROP':
+        new_size = (224, 224)
+
+        bbox = [
+                (img_orig_pil.size[0] - new_size[0])/2, 
+                (img_orig_pil.size[1] - new_size[1])/2,
+                (img_orig_pil.size[0] + new_size[0])/2,
+                (img_orig_pil.size[1] + new_size[1])/2,
+        ]
+
+        img_HR_pil = img_orig_pil.crop(bbox)
+        img_HR_np = pil_to_np(img_HR_pil)
+    else:
+        img_HR_pil, img_HR_np = img_orig_pil, img_orig_np
+        
+    LR_size = [
+               img_HR_pil.size[0] // factor, 
+               img_HR_pil.size[1] // factor
+    ]
+
+    img_LR_pil = img_HR_pil.resize(LR_size, Image.ANTIALIAS)
+    img_LR_np = pil_to_np(img_LR_pil)
+
+    print('HR and LR resolutions: %s, %s' % (str(img_HR_pil.size), str(img_LR_pil.size)))
+
+    return {
+                'orig_pil': img_orig_pil,
+                'orig_np':  img_orig_np,
+                'LR_pil':  img_LR_pil, 
+                'LR_np': img_LR_np,
+                'HR_pil':  img_HR_pil, 
+                'HR_np': img_HR_np
+           }
 
 def get_baselines(img_LR_pil, img_HR_pil):
     '''Gets `bicubic`, sharpened bicubic and `nearest` baselines.'''
